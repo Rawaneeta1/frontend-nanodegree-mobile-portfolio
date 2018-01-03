4f043c507dc25a304cda4f413d07eb8554459cbe
Rawan Hamad <Rawaneeta1@gmail.com>
2018-01-03 11:29:06 +0300

    refactor changeSliderLabel in main.js

--- views/js/main.js
+++ views/js/main.js
@@ -425,15 +425,16 @@ var resizePizzas = function(size) {
 
     // Changes the value for the size of the pizza above the slider
     function changeSliderLabel(size) {
+      var pizzaSize =  document.getElementById("pizzaSize");
         switch (size) {
             case "1":
-                document.getElementById("pizzaSize").innerHTML = "Small";
+                pizzaSize.innerHTML = "Small";
                 return;
             case "2":
-                document.getElementById("pizzaSize").innerHTML = "Medium";
+                pizzaSize.innerHTML = "Medium";
                 return;
             case "3":
-                document.getElementById("pizzaSize").innerHTML = "Large";
+                pizzaSize.innerHTML = "Large";
                 return;
             default:
                 console.log("bug in changeSliderLabel");
